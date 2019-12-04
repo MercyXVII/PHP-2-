05805495ad2cd2df49c95d679de79f6fc5d5c187
Martijn <127121@student.drenthecollege.nl>
2019-12-04 10:58:01 +0100

    file structure

--- opdracht_a/index.php
+++ opdracht_a/index.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
-  <html>
-    <head>
-      <title>Opdracht A</title>
-    </head>
-      <body>
-        <form name="calculator" action="calculator.php" method="post">
-          <input type="number" name="nummer1" placeholder="Nummer 1"><br>
-          <select name="operator">
-            <option value="kies"> Kies </option>
-            <option value="plus"> + </option>
-            <option value="min"> - </option>
-            <option value="keer"> x </option>
-            <option value="delen"> / </option>
-          </select><br>
-          <input type="number" name="nummer2" placeholder="Nummer 2"><br>
-          <input type="submit" name="submit" value="Enter"><br>
-            <?php
-            include('calculator.php');
-            ?>
-      </body>
-  </html>
+<html>
+  <head>
+    <title>Opdracht A</title>
+  </head>
+    <body>
+      <form action="calculator.php" method="POST">
+        <label>Select operation:</label>
+        <select name = "dropdown">
+          <option></option>
+          <option>+</option>
+          <option>-</option>
+          <option>*</option>
+          <option>/</option>
+        <select> <br> <br>
+          First number: <input type="number" name="firstNum" required><br><br>
+          Second number: <input type="number" name="secondNum" required><br><br>
+        <input type="submit" value = "Enter">
+      </form>  
+    </body>
+</html>
