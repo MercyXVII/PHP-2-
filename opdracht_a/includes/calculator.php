02cbdcea5fe8c76c7a95812717b4c24fe175b704
Martijn <127121@student.drenthecollege.nl>
2019-12-05 11:25:37 +0100

    Cleanup

--- opdracht_a/includes/calculator.php
+++ opdracht_a/includes/calculator.php
@@ -1,9 +1,13 @@
 <?php
+
+  error_reporting(0);
+  
   if(isset($_POST['submit'])){
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
     $calc = $_POST['calcu'];
-}
+  }
+
     function calc($num1, $num2, $calc){
       switch($calc){
         case "+":
@@ -23,5 +27,4 @@
       }
       return $result;
     }
-    echo "The answer is =". calc($num1, $num2, $calc);
 ?>
--- opdracht_a/index.php
+++ opdracht_a/index.php
@@ -8,13 +8,14 @@
         <input type="radio" value= "+" name="calcu"> +
         <input type="radio" value= "-" name="calcu"> -
         <input type="radio" value= "*" name="calcu"> *
-        <input type="radio" value= "/" name="calcu"> / <br>
+        <input type="radio" value= "/" name="calcu"> / <br> <br>
           First number: <input type="number" name="num1"><br><br>
           Second number: <input type="number" name="num2"><br><br>
-        <input type="submit" value="Enter">
+        <input type="submit" value="Enter" name="submit"><br> <br>
       </form>
         <?php
           include('includes/calculator.php');
+          echo "The answer is = ". calc($num1, $num2, $calc);
         ?>
     </body>
 </html>
